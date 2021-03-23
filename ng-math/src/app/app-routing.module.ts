7c8294a2bbd53fb2490ff39e4dbd39605df0ec51
unknown <holota@cmcdata.sk>
2021-03-23 16:40:54 +0100

    design update

--- ng-math/src/app/app-routing.module.ts
+++ ng-math/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './views/about/about.component';
 import { MathComponent } from './views/math/math.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/exercise', pathMatch: 'full' },
   { path: 'exercise', component: MathComponent },
   { path: 'about', component: AboutComponent },
 ];
